2c89394a1cfe2005da80d733e1c140787f611e6a
yeganehlorigoeeni <yeganehlorigoeeni.8@gmail.com>
2024-11-26 12:09:27 +0330

    ADD Rotes and listcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { AddComponent } from './pages/add/add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'list',pathMatch:'full'},
+    {path:'list',component:ListComponent},
+    {path:'add',component:AddComponent}
+];
